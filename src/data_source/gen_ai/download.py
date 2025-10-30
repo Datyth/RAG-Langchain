219d810965005436f41d9b812a598328272cefe6
Phat Dat <tophatdat.160305@gmail.com>
2025-10-30 14:14:19 +0700

    update

--- src/data_source/gen_ai/download.py
+++ src/data_source/gen_ai/download.py
@@ -9,19 +9,19 @@ file_links = [
     {
          "title": "BERT- Pre-training of Deep Bidirectional Transformers for Language Understanding",
          "url": "https://arxiv.org/pdf/1810.04805"
-    }
+    },
     {
         "title": "Chain-of-Thought Prompting Elicits Reasoning in Large Language Models",
         "url": "https://arxiv.org/pdf/2201.11903"
-    }
+    },
     {
         "title": "Denoising Diffusion Probabilistic Models",
         "url": "https://arxiv.org/pdf/2006.11239"
-    }
+    },
     {
         "title": "Instruction Tuning for Large Language Models- A Survey",
         "url": "https://arxiv.org/pdf/2308.10792"
-    }
+    },
     {
         "title": "Llama 2- Open Foundation and Fine-Tuned Chat Models",
         "url": "https://arxiv.org/pdf/2307.09288"
